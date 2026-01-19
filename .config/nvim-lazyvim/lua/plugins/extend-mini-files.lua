81417582d50f79b678b29661bec5236a579b5e4d
Ralph Smith <devralph@fastmail.com>
2026-01-19 13:23:24 -0700

    fix: add nvvim-lazyvim config

---
+++ .config/nvim-lazyvim/lua/plugins/extend-mini-files.lua
@@ -0,0 +1,44 @@
+return {
+  "nvim-mini/mini.files",
+  lazy = false,
+  keys = {
+    {
+      "<leader>e",
+      function()
+        require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+      end,
+      desc = "Open mini.files (directory of current file)",
+    },
+    {
+      "<leader>E",
+      function()
+        require("mini.files").open(vim.uv.cwd(), true)
+      end,
+      desc = "Open mini.files (cwd)",
+    },
+    {
+      "<leader>fm",
+      function()
+        require("mini.files").open(LazyVim.root(), true)
+      end,
+      desc = "Open mini.files (root)",
+    },
+  },
+  opts = {
+    options = {
+      use_as_default_explorer = true,
+    },
+    windows = {
+      -- Maximum number of windows to show side by side
+      max_number = math.huge,
+      -- Whether to show preview of file/directory under cursor
+      preview = true,
+      -- Width of focused window
+      width_focus = 40,
+      -- Width of non-focused window
+      width_nofocus = 20,
+      -- Width of preview window
+      width_preview = 70,
+    },
+  },
+}

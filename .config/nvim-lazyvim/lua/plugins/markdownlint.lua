b5e266a357c358083a14dab7f0b4e8f2786a2b26
Ralph Smith <devralph@fastmail.com>
2026-01-20 18:39:38 -0700

    fix: update nvim config

--- .config/nvim-lazyvim/lua/plugins/markdownlint.lua
+++ .config/nvim-lazyvim/lua/plugins/markdownlint.lua
@@ -4,8 +4,7 @@ local config = {
   },
 }
 
---local config_path = vim.fn.stdpath("config") .. "/.markdownlint.json"
-local config_path = ".markdownlint.json"
+local config_path = vim.fn.stdpath("config") .. "/.markdownlint.json"
 local file = io.open(config_path, "w")
 if file then
   file:write(vim.json.encode(config))
